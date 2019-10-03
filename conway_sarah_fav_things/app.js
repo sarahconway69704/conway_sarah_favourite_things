073cab89551fff00b1cc881fcae4eff8cbc110ff
sarahconway69704 <35806843+sarahconway69704@users.noreply.github.com>
2019-10-02 20:07:44 -0400

    Add files via upload

---
+++ conway_sarah_fav_things/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path'); // path lets us navigate the file system / folders
+
+// heroku assigns a port when it deploys via the process (environment variables, coming soon )
+// locally this will run @ port 3000; remotely it'll run wherever heroku tells it to run
+const port = process.env.PORT || 3000; //double pipe means "or"
+
+const app = express();
+
+app.use(express.static('public'));
+
+// a forward slash is the home route (same as index.html)
+app.get('/', (req, res) => {
+  console.log('at the home route');
+  res.sendfile(path.join(__dirname + '/views/index.html'));
+  // this builds localhost:3000/views/index.html
+})
+
+
+app.get('/contact', (req, res) => {
+  res.sendfile(path.join(__dirname + '/views/contact.html'));
+  console.log('at the contact route');
+})
+
+app.get('/portfolio', (req, res) => {
+  res.send('on the portfolio page');
+  console.log('at the portfolio route');
+})
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
+})
